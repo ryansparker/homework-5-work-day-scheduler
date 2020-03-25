f1072e00d98c034758490c5e3707f5073bf698db
Ryan Parker <ryansparker@gmail.com>
2020-03-25 18:59:55 -0400

    syled buttons more, organized code, experiementing with local storage

--- src/script.js
+++ src/script.js
@@ -1,6 +1,48 @@
+
+//Display Date on heading
 function setDateTime() {
     var dateTimeP = $("#currentDay");
 
     const today = moment();
     dateTimeP.text(today.format("dddd" + ", " + "MMMM" + " " + "DD" + ", " + "YYYY"));
 }
+
+setDateTime();
+
+
+//inputting info into local storage
+
+var inputInfo = document.getElementById("input9am");
+var saveInfo = document.getElementById("btn9am");
+var lsDisplay = $("#lsOutput");
+
+saveInfo.onclick = function() {
+    var info = inputInfo.value;
+
+    console.log(info);
+
+    if (info) {
+        localStorage.setItem(info);
+        location.reload();
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+//Moments' notes
+
+// moment().isAfter("GMT-0400")
+// moment().isBefore("GMT-0400")
+// moment().isSame("GMT-0400")
+
+// var d = moment.duration(3, "days")
+// console.log(d);
