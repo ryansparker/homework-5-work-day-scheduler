f0686a5ffdefc335600850ed2bdf371dd8203ac4
Ryan Parker <ryansparker@gmail.com>
2020-03-26 09:22:16 -0400

    shaped up the html more by removing unneeded divs in all form rows. added all of the times for each row for the localStorage. adjusted styling.

--- src/script.js
+++ src/script.js
@@ -14,12 +14,27 @@ setDateTime();
 
 var input9am = document.getElementById("input9am");
 var btn9am = document.getElementById("btn9am");
+var input10am = document.getElementById("input10am");
+var btn10am = document.getElementById("btn10am");
+var input11am = document.getElementById("input11am");
+var btn11am = document.getElementById("btn11am");
+var input12pm = document.getElementById("input12pm");
+var btn12pm = document.getElementById("btn12pm");
+var input1pm = document.getElementById("input1pm");
+var btn1pm = document.getElementById("btn1pm");
+var input2pm = document.getElementById("input2pm");
+var btn2pm = document.getElementById("btn2pm");
+var input3pm = document.getElementById("input3pm");
+var btn3pm = document.getElementById("btn3pm");
+var input4pm = document.getElementById("input4pm");
+var btn4pm = document.getElementById("btn4pm");
+var input5pm = document.getElementById("input5pm");
+var btn5pm = document.getElementById("btn5pm");
 
 $(".saveBtn").on("click", function (event) {
     event.preventDefault();
     var textArea = $(this).siblings("textarea").val();
     var key = $(this).attr("id");
-    // var scheduleInput = input9am.value.trim();
 
     console.log(textArea);
 
@@ -29,7 +44,7 @@ $(".saveBtn").on("click", function (event) {
 $("#input9am").val(localStorage.getItem("btn9am"));
 
 function updateBg() {
-    var currentHour = moment().hours()-6;
+    var currentHour = moment().hours();
     $(".row").each(function () {
         var blockHour = parseInt($(this).attr("id"));
         if (blockHour < currentHour) {
