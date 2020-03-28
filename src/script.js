6f598693f2408c824d456abcde7c2f551a6ed0f6
Ryan Parker <ryansparker@gmail.com>
2020-03-28 15:30:20 -0400

    replaced reg javascript with jquery

--- src/script.js
+++ src/script.js
@@ -12,24 +12,24 @@ setDateTime();
 //----------------------------------
 //LOCAL STORAGE
 //defining variable referencing HTML
-var input9am = document.getElementById("input9am");
-var btn9am = document.getElementById("btn9am");
-var input10am = document.getElementById("input10am");
-var btn10am = document.getElementById("btn10am");
-var input11am = document.getElementById("input11am");
-var btn11am = document.getElementById("btn11am");
-var input12pm = document.getElementById("input12pm");
-var btn12pm = document.getElementById("btn12pm");
-var input1pm = document.getElementById("input1pm");
-var btn1pm = document.getElementById("btn1pm");
-var input2pm = document.getElementById("input2pm");
-var btn2pm = document.getElementById("btn2pm");
-var input3pm = document.getElementById("input3pm");
-var btn3pm = document.getElementById("btn3pm");
-var input4pm = document.getElementById("input4pm");
-var btn4pm = document.getElementById("btn4pm");
-var input5pm = document.getElementById("input5pm");
-var btn5pm = document.getElementById("btn5pm");
+var input9am = $("#input9am");
+var btn9am = $("#btn9am");
+var input10am = $("#input10am");
+var btn10am = $("#btn10am");
+var input11am = $("#input11am");
+var btn11am = $("#btn11am");
+var input12pm = $("#input12pm");
+var btn12pm = $("#btn12pm");
+var input1pm = $("#input1pm");
+var btn1pm = $("#btn1pm");
+var input2pm = $("#input2pm");
+var btn2pm = $("#btn2pm");
+var input3pm = $("#input3pm");
+var btn3pm = $("#btn3pm");
+var input4pm = $("#input4pm");
+var btn4pm = $("#btn4pm");
+var input5pm = $("#input5pm");
+var btn5pm = $("#btn5pm");
 
 //save button to input info into rows
 $(".saveBtn").on("click", function (event) {
